3194dc068bb6e64d6c316b2770c377220c84f0eb
pankajkumar24-dev <pkumargithub@gmail.com>
2026-09-13 10:36:50 +0530

    Program change

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ if (app.Environment.IsDevelopment())
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Swelteringgg", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
